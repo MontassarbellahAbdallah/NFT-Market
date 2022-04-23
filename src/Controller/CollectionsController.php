c2b61924e0087f5a824a9ff6ca61a8d08afe07cd
AmirSouissi <a.souissi@ewm.swiss>
2022-04-23 14:21:32 +0100

    fix Request bug

--- src/Controller/CollectionsController.php
+++ src/Controller/CollectionsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Collect;
 class CollectionsController extends AbstractController
